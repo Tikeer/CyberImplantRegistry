2889502445364ec53342aa23094c73360f6e3cd6
Tikeer <sebastian.dmochowski12@gmail.com>
2026-01-04 22:06:32 +0100

    zmiana wywolania

--- src/main.c
+++ src/main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include "implant_def.h"
-#include "list_manager.h"
-#include <string.h>
-#include <time.h>
+#include "file_io.h"
 
 #include "ui.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         printf("Nie podano zadnych argumentow \n");
-        printf("Instrukcja: %s <sciezka_do_pliku>\n", argv[0]);
+        printf("Instrukcja: %s <sciezka_do_bazy_danych>\n", argv[0]);
         return 1;
     }
 
     char* db_path = argv[1];
     Node* head = NULL;
 
+    load_database(db_path,&head);
+
     while (1) {
-        print_menu(head);
-    }
+        if (print_menu(&head,db_path) == 0) {
+            break;
+            }
+        }
+    return 0;
 }
