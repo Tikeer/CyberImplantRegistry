bf2c5e6b4b920ef14ec794fb4d5743c997859aec
Tikeer <sebastian.dmochowski12@gmail.com>
2026-01-04 17:26:25 +0100

    poprawienie logiki

--- include/logic.h
+++ include/logic.h
@@ -2,5 +2,6 @@
 #define PROJEKT_KONCOWY_PP_LOGIC_H
 
 int validate_implant_rules(Implant data);
+void find_implant(Node** head_ref,char* search);
 
 #endif //PROJEKT_KONCOWY_PP_LOGIC_H
--- src/logic.c
+++ src/logic.c
@@ -1,5 +1,6 @@
 #include "list_manager.h"
 #include "implant_def.h"
+#include "ui.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,51 +13,81 @@ int validate_implant_rules(Implant data) {
     }
 
     if (data.energy > 85.0 && data.status == LEGAL) {
+        data.status = ILLEGAL;
         printf("Moc energi zbyt duza, zmienianie statusu\n");
         return 0;
     }
     return 1;
 }
 
-void validate_number(char y) {
-    int found=0;
-
-    do {
-        for (int i = 0; i < y;i++) {
-            if (isdigit(y)) {
-                found=1;
-            }
-        }
-
-    }while (!found);
-}
-
-void validate_string(char* x) {
+void find_implant(Node** head_ref,char* search) {
+    Node* current = *head_ref;
     int found = 0;
 
-    do {
-        for (int i = 0; i < strlen(x); ++i) {
-            if (isalpha(x[i])) {
-                found = 1;
+    if (current == NULL) {
+        return;
+    }
 
-            }
+    while (current != NULL) {
+        //wyszukiwanie po nazwie implantu
+        if (strcmp(current->data.name, search) == 0) {
+            show_implant_data(current->data);
+            found = 1;
         }
-    }while(!found);
+        //wyszukiwanie po id
+        if (strcmp(current->data.id, search) == 0) {
+            show_implant_data(current->data);
+            found = 1;
+        }
+        //wyszukwianie po producencie
+        if (strcmp(current->data.developer, search) == 0) {
+            show_implant_data(current->data);
+            found = 1;
+        }
+        current = current->next;
+    }
+    if (found == 0) {
+        printf("Nie znaleziono.\n");
+    }
+
 }
 
-void validate_implant(Node** head_ref) {
+stats count_illegal(Node** head_ref) {
+    Node* current = *head_ref;
+    stats s = {0,0,0};
+
+    while (current != NULL) {
+        if (current->data.status == LEGAL) {
+            s.legal_counter++;
+        }
+        if (current->data.status == ILLEGAL) {
+            s.illegal_counter++;
+        }
+        if (current->data.status == GRAY_AREA) {
+            s.gray_area_counter++;
+        }
+    }
+
+    return s;
+}
+
+void try_delete_implant(Node** head_ref,char* try_to_delete) {
     Node* current = *head_ref;
 
-}
+    if (current == NULL) {
+        return;
+    }
 
-void find_implant() {
-
-}
-
-void count_illegal() {
-
-}
-
-void try_delete_implant() {
-
+    while (current != NULL) {
+        if (strcmp(current->data.id, try_to_delete) == 0) {
+            if (current->data.status == ILLEGAL) {
+                printf("NIE MOZNA USUNAC DOWOD W SPRAWIE");
+                return;
+            }else {
+                delete_node(head_ref, try_to_delete);
+            }
+        }else {
+            printf("Nie znaleziono.\n");
+        }
+    }
 }
